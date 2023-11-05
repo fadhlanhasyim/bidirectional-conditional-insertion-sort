af64354c03f7bada128ba7d2a32390d97b42c658
fadhlanhasyim <fadhlanhasyim54@gmail.com>
2023-11-06 00:40:39 +0700

    finalized

--- dataset_generator.py
+++ dataset_generator.py
@@ -5,8 +5,9 @@ class DatasetGenerator:
     __MEDIUM_N = 5_000
     __LARGE_N = 50_000
 
-    def write(filename, list_data):
-        with open('dataset' + filename, 'w') as file:
+    @classmethod
+    def write_file(self, filename, list_data):
+        with open('dataset/' + filename, 'w') as file:
             for item in list_data:
                 file.write(str(item) + '\n')
 
@@ -31,46 +32,55 @@ class DatasetGenerator:
     @classmethod
     def generate_small_random_list(self):
         small_random_list = self.__generate_random_list(self.__SMALL_N)
+        self.write_file('small_random.txt', small_random_list)
         return small_random_list
     
     @classmethod
     def generate_medium_random_list(self):
         medium_random_list = self.__generate_random_list(self.__MEDIUM_N)
+        self.write_file('medium_random.txt', medium_random_list)
         return medium_random_list
     
     @classmethod
     def generate_large_random_list(self):
         large_random_list = self.__generate_random_list(self.__LARGE_N)
+        self.write_file('large_random.txt', large_random_list)
         return large_random_list
     
     #  SORTED
     @classmethod
     def generate_small_sorted_list(self):
         small_sorted_list = self.__generate_sorted_list(self.__SMALL_N)
+        self.write_file('small_sorted.txt', small_sorted_list)
         return small_sorted_list
     
     @classmethod
     def generate_medium_sorted_list(self):
         medium_sorted_list = self.__generate_sorted_list(self.__MEDIUM_N)
+        self.write_file('medium_sorted.txt', medium_sorted_list)
         return medium_sorted_list
     
     @classmethod
     def generate_large_sorted_list(self):
         large_sorted_list = self.__generate_sorted_list(self.__LARGE_N)
+        self.write_file('large_sorted.txt', large_sorted_list)
         return large_sorted_list
     
     # REVERSED LIST
     @classmethod
     def generate_small_reversed_list(self):
         small_reversed_list = self.__generate_reversed_list(self.__SMALL_N)
+        self.write_file('small_reversed.txt', small_reversed_list)
         return small_reversed_list
     
     @classmethod
     def generate_medium_reversed_list(self):
         medium_reversed_list = self.__generate_reversed_list(self.__MEDIUM_N)
+        self.write_file('medium_reversed.txt', medium_reversed_list)
         return medium_reversed_list
     
     @classmethod
     def generate_large_reversed_list(self):
         large_reversed_list = self.__generate_reversed_list(self.__LARGE_N)
+        self.write_file('large_reversed.txt', large_reversed_list)
         return large_reversed_list
